06c278aaedb91e0f2336753e012a869ec8d2c305
griffin <gfluet5@gmail.com>
2022-04-03 19:18:43 -0400

    Tweaks
    small changes to smirking

--- client.py
+++ client.py
@@ -2,9 +2,8 @@ import discord
 import json
 
 tokens = json.load(open(file="./tokens.json", encoding="utf-8"))
-config = json.load(open(file="./config.json", encoding="utf-8"))
 
-game = discord.Game(config["game"])
+game = discord.Game("Five Nights at Freddy's")
 client = discord.Client(status=discord.Status.online, activity=game)
 
 @client.event
@@ -14,19 +13,21 @@ async def on_ready():
 @client.event
 async def on_message(message):
     print("On message called")
-    # If message came from bot, ignore it and return
+    # If message came from client, ignore it and return
     if message.author == client.user:
-        print("Bot message, ignoring")
+        print("client message, ignoring")
         return
-    # If message starts with "$hello", bot responds
+    # If message starts with "$hello", client responds
     if message.content.startswith("$hello"):
         await message.channel.send("Hello!")
         print("Sending Hello")
     
     # Whenever a post is made in selected channel, reacts with smirk
-    if str((message.channel)) == config["smirking_channel"]:
+    if str((message.channel)) == "memes😏":
         await message.add_reaction("😏")
-        print("Smirking post in {channel}".format(channel = config["smirking_channel"]))
+        print("Message from {0.author.name} in {0.guild.name} getting smirked 😏 \nMessage contents : {0.content}"
+            .format(message))
+
 
 
 client.run(tokens["token"])
